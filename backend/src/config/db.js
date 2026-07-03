ab939c58f0f6ca3cc99faccad5320bd9ea6752fb
zanydrew <nizahon00@gmail.com>
2026-07-03 20:36:35 +0700

    fix connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_POR,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
     define: {
